5db76212e037da8c139703f916e13519fe571aa2
Akos Szente <akos.szente@egroup.hu>
2015-11-29 04:32:43 +0100

    setupGeo function

--- eatquick.js
+++ eatquick.js
@@ -22,44 +22,48 @@ $(document).ready(function($){
             size: 4
         });
 
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(geo) {
-              if (geo.coords)
-              {
-                  var lon = geo.coords.longitude; 
-                  var lat = geo.coords.latitude; 
-
-                  jQuery.ajax("http://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&sensor=true", {
-                    type: "GET",
-                    async: true
-                    }) .success(function( data ) {
-                      try{
-                        var found = false; 
-                        var postcode = null; 
-                        for (var i = 0; !found && i < data.results.length; i++) {
-                            //console.log(data.results[i].address_components);
-                            for(var j = 0; !found && j < data.results[i].address_components.length; j++){
-                              if(data.results[i].address_components[j].types[0] == "postal_code"){
-                                console.log(data.results[i].address_components[j].short_name);
-                                postcode = data.results[i].address_components[j].short_name; 
-                                found = true; 
-                              }
-                            }
-                        }
-
-                        if(found && postcode != null){
-                          $('#locationInput').val(postcode);
-                        }
-                      } catch(err){
-                          console.log("There was a problem with postcode resolution.");
-                      }
-                    }) .error(function() {
-                        console.log("Unable to get a valid response from Google Maps at postcode resolution.");
-                  });
-              }
-          });
-         }
-        else {
-          console.log("Geolocation API is not supported"); 
-        }      
+        setupGeo('#locationInput'); 
     });
+
+function setupGeo(target){
+  if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(geo) {
+        if (geo.coords)
+        {
+            var lon = geo.coords.longitude; 
+            var lat = geo.coords.latitude; 
+
+            jQuery.ajax("http://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&sensor=true", {
+              type: "GET",
+              async: true
+              }) .success(function( data ) {
+                try{
+                  var found = false; 
+                  var postcode = null; 
+                  for (var i = 0; !found && i < data.results.length; i++) {
+                      //console.log(data.results[i].address_components);
+                      for(var j = 0; !found && j < data.results[i].address_components.length; j++){
+                        if(data.results[i].address_components[j].types[0] == "postal_code"){
+                          console.log(data.results[i].address_components[j].short_name);
+                          postcode = data.results[i].address_components[j].short_name; 
+                          found = true; 
+                        }
+                      }
+                  }
+
+                  if(found && postcode != null){
+                    $(target).val(postcode);
+                  }
+                } catch(err){
+                    console.log("There was a problem with postcode resolution.");
+                }
+              }) .error(function() {
+                  console.log("Unable to get a valid response from Google Maps at postcode resolution.");
+            });
+        }
+    });
+   }
+  else {
+    console.log("Geolocation API is not supported"); 
+  }      
+}
